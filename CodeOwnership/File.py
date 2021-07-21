6675eb2fef425e25ba3acd7f85240a67dc2096c9
MashiroCl <441016116@qq.com>
2021-07-21 16:07:14 +0900

    Function counting commit author ratio, writing result into csv files finished

--- CodeOwnership/File.py
+++ CodeOwnership/File.py
@@ -11,6 +11,7 @@ class File():
         self.commits=None
         self.commitNum=0
         self.authorCommitDict=dict()
+        self.authorCommitDictRatio=dict()
     def setParentPath(self)->str:
         paths=self.path.split("/")
         temp=""
@@ -19,17 +20,12 @@ class File():
         self.parentPath=temp
         return temp
 
-    def createFolder(self,path:str):
-        pass
-
     def getCommit(self,outputPath:str):
         'git log on current java file and ouput a json format log'
         self.setParentPath()
         self.log=outputPath+"/"+self.name.split(".")[0]+".json"
-        print(self.log)
         prettyFormat="--pretty=format:\'{%n  \"commit\": \"%H\",%n \"subject\": \"%s\",%n \"author\": {%n    \"name\": \"%aN\",%n    \"email\": \"%aE\",%n    \"date\": \"%aD\"%n  },%n  \"commiter\": {%n    \"name\": \"%cN\",%n    \"email\": \"%cE\",%n    \"date\": \"%cD\"%n  }%n},\'"
         command="cd "+self.parentPath+" && "+"git log "+prettyFormat+" --follow "+self.name +" >"+ self.log
-        print(command)
         os.system(command)
 
     def json2Commit(self):
@@ -49,10 +45,18 @@ class File():
             else:
                 self.authorCommitDict[each.authorName] =1
 
+
+    def commitAuthorRatio(self)->dict:
+        for each in self.authorCommitDict:
+            self.authorCommitDictRatio[each]=self.authorCommitDict[each]/self.commitNum
+
+
 if __name__ =="__main__":
     path="/Users/leichen/ResearchAssistant/InteractiveRebase/data/mbassador/src/main/java/net/engio/mbassy/bus/BusRuntime.java"
     f=File(path)
     f.getCommit(outputPath="/Users/leichen/Code/pythonProject/pythonProject/salabResearch/CodeOwnership")
-    commits=f.json2Commit()
+    f.json2Commit()
     f.commitAuthorCount()
+    f.commitAuthorRatio()
     print(f.authorCommitDict)
+    print(f.authorCommitDictRatio)
--- CodeOwnership/Repository.py
+++ CodeOwnership/Repository.py
@@ -1,6 +1,46 @@
+from File import File
+import glob
+import csv
 class Repository():
     def __init__(self,path:str):
         self.path=path
-
+        self.name=path.split("/")[-1]
+        self.files=[]
     def findJavaFiles(self)->list:
-        pass
+        javaFiles=glob.glob(self.path+'/**/*.java',recursive=True)
+        for each in javaFiles:
+            self.files.append(File(each))
+
+    def countAuthorCommit(self,outputPath:str):
+        self.findJavaFiles()
+        for each in self.files:
+            each.getCommit(outputPath)
+            each.json2Commit()
+            each.commitAuthorCount()
+            each.commitAuthorRatio()
+
+        # for each in self.files:
+        #     print(each.name)
+        #     print(each.authorCommitDictRatio)
+
+    def writeCSV(self,outputPath:str):
+        result=[]
+        for eachFile in self.files:
+            for each2 in eachFile.authorCommitDictRatio:
+                temp = []
+                temp.append(eachFile.path)
+                temp.append(eachFile.name)
+                temp.append(each2)
+                temp.append(eachFile.authorCommitDictRatio[each2])
+                result.append(temp)
+        with open(str(outputPath+"/"+self.name+".csv"),"w") as csvfile:
+            writer=csv.writer(csvfile)
+            writer.writerow(["File Path","File Name","Author Name","Ownership"])
+            writer.writerows(result)
+
+if __name__=="__main__":
+    path="/Users/leichen/ResearchAssistant/InteractiveRebase/data/mbassador"
+    outputPath="/Users/leichen/ResearchAssistant/InteractiveRebase/data/mbassador/outputTest"
+    repo=Repository(path)
+    repo.countAuthorCommit(outputPath=outputPath)
+    repo.writeCSV("/Users/leichen/ResearchAssistant/InteractiveRebase/data/mbassador/csv")
