bd8debb044bd8452b79c93c41c3e13b742b541c2
MashiroCl <441016116@qq.com>
2021-07-26 10:43:48 +0900

    class Repository add method getownership which can obtain ownership info from csv using file path

--- CodeOwnership/Repository.py
+++ CodeOwnership/Repository.py
@@ -6,6 +6,7 @@ class Repository():
         self.path=path
         self.name=path.split("/")[-1]
         self.files=[]
+        self.csvPath=""
     def findJavaFiles(self)->list:
         javaFiles=glob.glob(self.path+'/**/*.java',recursive=True)
         for each in javaFiles:
@@ -33,10 +34,23 @@ class Repository():
                 temp.append(each2)
                 temp.append(eachFile.authorCommitDictRatio[each2])
                 result.append(temp)
-        with open(str(outputPath+"/"+self.name+".csv"),"w") as csvfile:
+        csvPath=str(outputPath+"/"+self.name+".csv")
+        with open(csvPath,"w") as csvfile:
             writer=csv.writer(csvfile)
             writer.writerow(["File Path","File Name","Author Name","Ownership"])
             writer.writerows(result)
+        self.csvPath=csvPath
+
+
+    def getOwnership(self,filePath:str)->list:
+        with open(self.csvPath) as f:
+            reader=csv.reader(f)
+            rows=[row for row in reader]
+        result=[]
+        for each in rows[1:]:
+            if each[0]==filePath:
+                result.append(each)
+        return result
 
 if __name__=="__main__":
     path="/Users/leichen/ResearchAssistant/InteractiveRebase/data/mbassador"
@@ -44,3 +58,15 @@ if __name__=="__main__":
     repo=Repository(path)
     repo.countAuthorCommit(outputPath=outputPath)
     repo.writeCSV("/Users/leichen/ResearchAssistant/InteractiveRebase/data/mbassador/csv")
+    filePath="/Users/leichen/ResearchAssistant/InteractiveRebase/data/mbassador/src/main/java/net/engio/mbassy/common/AbstractConcurrentSet.java"
+    result=repo.getOwnership(filePath)
+    print(result)
+    '''
+    user输入名字，存储到solution中作为一个值solution.user
+    然后RO涉及到的两个文件中是否有solution.user对应的ownership
+        有的话就在evaluation里当一个值,
+        没有的话就是0
+    
+    class jClass里需要有java文件路径信息
+    
+    '''
