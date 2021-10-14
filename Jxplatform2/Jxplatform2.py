a534f48fa7a63717d9116d33a656a0607ffd387c
MashiroCl <441016116@qq.com>
2021-10-14 14:10:08 +0900

    new Jxplatform2Extract jar file and its command added

--- Jxplatform2/Jxplatform2.py
+++ Jxplatform2/Jxplatform2.py
@@ -1,4 +1,3 @@
-from utils import create_folder
 import os
 class Jxplatform2:
     def __init__(self,jxplatPath:str,target:str,classPath:str,outputPath:str):
@@ -7,14 +6,14 @@ class Jxplatform2:
         self.classPath=classPath
         self.outputPath=outputPath
     def extractInfo(self):
-        command="java -jar "+self.jxplatPath+" "+self.target + " "+self.classPath+" "+ self.outputPath
+        command="java -jar "+self.jxplatPath+" "+self.target +" "+ self.outputPath
         print(command)
         os.system(command)
 
 if __name__ == "__main__":
     j="/Users/leichen/Code/pythonProject/pythonProject/salabResearch/Jxplatform2/JxplatformExtract.jar"
-    t="/Users/leichen/ResearchAssistant/InteractiveRebase/data/mbassador"
+    t="/Users/leichen/ResearchAssistant/InteractiveRebase/data/jedis"
     c=t
-    outputPath="/Users/leichen/ResearchAssistant/InteractiveRebase/data/mbassador/jmbassador.json"
+    outputPath="/Users/leichen/Desktop/jedis.json"
     jx=Jxplatform2(j,t,c,outputPath)
     jx.extractInfo()
