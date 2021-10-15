2ca9f7a96f9f471a9e48313c293c8a3261f31fbd
MashiroCl <441016116@qq.com>
2021-10-15 21:48:52 +0900

    encoding dict built

---
+++ Encoding/BinaryEncoding.py
@@ -0,0 +1,72 @@
+from .EncodingDict import EncodingDict
+class BinaryEncoding():
+    def __init__(self):
+        self.chromosomeLen=0
+        self.ROtypeLen = 3
+        self.classLen = 0
+        self.fieldLen = 0
+        self.methodLen = 0
+        self.encodingDict = EncodingDict()
+    def encoding(self,projectInfo)->str:
+        '''
+        Encode a list of jClass entities into binary sequences which represent refactoring operations
+        :param projectInfo: list of jClass entities
+        :return: chromosome length after encoding
+        '''
+        'Element: ROtype length:3'
+
+        'calculate length of longest '
+        jFieldLenMax = -1
+        jMethodLenMax = -1
+        for eachClass in projectInfo:
+            jFieldLenTemp = len(eachClass.getField())
+            jMethodLenTemp = len(eachClass.getMethod())
+            if(jFieldLenTemp>jFieldLenMax):
+                    jFieldLenMax = jFieldLenTemp
+            if(jMethodLenTemp>jMethodLenMax):
+                    jMethodLenMax =  jMethodLenTemp
+        'calculate binary sequence length'
+        '-1 because encode from 0 not 1'
+        jFieldLenMax = len(bin(jFieldLenMax-1).split("0b")[1])
+        jMethodLenMax = len(bin(jMethodLenMax-1).split("0b")[1])
+        jClassEntitiesLen = len(bin(len(projectInfo)-1).split("0b")[1])
+
+        self.classLen=jClassEntitiesLen
+        self.methodLen=jMethodLenMax
+        self.fieldLen=jFieldLenMax
+
+        countJClass = 0
+        for eachClass in projectInfo:
+            jClass2 = self.toBinarySequence(countJClass,jClassEntitiesLen)
+            countJClass+=1
+            self.encodingDict.addClass(jClass2,eachClass)
+
+            countJField = 0
+            for eachField in eachClass.getField():
+                jField2 = self.toBinarySequence(countJField,jFieldLenMax)
+                countJField+=1
+                self.encodingDict.addField(jClass2,jField2,eachField)
+
+            countJMethod = 0
+            for eachMethod in eachClass.getMethod():
+                jMethod2 = self.toBinarySequence(countJMethod,jMethodLenMax)
+                countJMethod+=1
+                self.encodingDict.addMethod(jClass2,jMethod2,eachMethod)
+
+        self.chromosomeLen = self.ROtypeLen + self.classLen*2 + self.fieldLen*2 + self.methodLen*2
+        return self.chromosomeLen
+
+    def decoding(self):
+        pass
+
+    def toBinarySequence(self,value,length):
+        '''
+        Convert an int number value into a binary sequence with a length of length
+        :param value: value being converted
+        :param length: binary sequence
+        :return: a string of binary sequence
+        '''
+        body = bin(value).split("0b")[1]
+        head = (length - len(body)) * "0"
+        result = head + body
+        return result
---
+++ Encoding/EncodingDict.py
@@ -0,0 +1,15 @@
+class EncodingDict():
+    def __init__(self):
+        self.encodingDict = {"class":{}}
+    def addClass(self,value,classInfo):
+        self.encodingDict["class"][value]={"classInfo":classInfo,
+                                           "field":{},
+                                           "method":{}}
+    def addField(self,classValue,fieldValue,fieldInfo):
+        self.encodingDict["class"][classValue]["field"][fieldValue]=fieldInfo
+
+    def addMethod(self,classValue,methodValue,methodInfo):
+        self.encodingDict["class"][classValue]["method"][methodValue]=methodInfo
+
+    def getEncodingDict(self):
+        return self.encodingDict
