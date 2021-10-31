a6d98febf65cba674d524fc7144c917d68ba4af5
MashiroCl <441016116@qq.com>
2021-10-31 16:06:50 +0900

    trival change to enum number

--- RefactoringOperation/RefactoringOperationEnum.py
+++ RefactoringOperation/RefactoringOperationEnum.py
@@ -1,12 +1,12 @@
 import enum
+
+
 class RefactoringOperationEnum(enum.Enum):
     INCLINECLASS = 1
     MOVEMETHOD = 2
     MOVEFIELD = 3
     PUSHDOWNMETHOD = 4
     PUSHDOWNFIELD = 5
-    PULLUPMETHOD =6
-    PULLUPFIELD =7
+    PULLUPMETHOD = 6
+    PULLUPFIELD = 7
     NULL = 8
-
-
