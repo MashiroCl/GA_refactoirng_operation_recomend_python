7091579659da3749471ad3dbc85e07adbda7fcd9
MashiroCl <441016116@qq.com>
2022-01-10 20:24:45 +0900

    Feat: test monoobjective for Effectiveness

---
+++ jMetal/NSGAIIMonoObjective.py
@@ -0,0 +1,64 @@
+from jmetal.algorithm.singleobjective.genetic_algorithm import GeneticAlgorithm
+import sys
+sys.path.append('../')
+from jmetal.operator import IntegerPolynomialMutation
+from jmetal.operator.crossover import IntegerSBXCrossover
+from jmetal.util.solution import get_non_dominated_solutions, print_function_values_to_file, print_variables_to_file
+from jmetal.algorithm.multiobjective import NSGAII
+from jmetal.util.termination_criterion import StoppingByEvaluations
+from utils import readJson
+from Jxplatform2.jClass import jClass
+from monoObjectiveRS import MonoObjectiveRS
+from jmetal.lab.visualization import Plot,InteractivePlot
+from jmetal.util.observer import WriteFrontToFileObserver,BasicObserver
+
+repoName = sys.argv[1]
+max_evaluations = sys.argv[2]
+platform = sys.argv[3]
+
+if platform == "1":
+    'Local'
+    max_evaluations = 500
+    repoName = "ganttproject-1.10.2"
+    jsonFile = "/Users/leichen/Desktop/" +repoName +".json"
+    repoPath = "/Users/leichen/ResearchAssistant/InteractiveRebase/data/" + repoName
+    outputPath = "/Users/leichen/Desktop/output"
+
+elif platform == "2":
+    'Server'
+    jsonFile = "/home/chenlei/MORCO/extractResult/" + repoName + ".json"
+    repoPath = "/home/chenlei/MORCO/data/" + repoName
+    outputPath = "/home/chenlei/MORCO/output_all_objectives_positive/"
+
+
+load = readJson(jsonFile)
+jClist = []
+for each in load:
+    jClist.append(jClass(load=each))
+# print(jClist)
+
+problem = MonoObjectiveRS(jClist,repoPath)
+
+# max_evaluations=5000
+algorithm = GeneticAlgorithm(
+    problem=problem,
+    population_size=50,
+    offspring_population_size=50,
+    mutation=IntegerPolynomialMutation(probability=1.0/problem.number_of_variables),
+    crossover=IntegerSBXCrossover(probability=1.0),
+    termination_criterion=StoppingByEvaluations(max_evaluations=int(max_evaluations))
+)
+algorithm.observable.register(observer=BasicObserver())
+algorithm.observable.register(observer=WriteFrontToFileObserver(
+    output_directory=outputPath+repoName+"/front"))
+algorithm.run()
+
+front = algorithm.get_result()
+
+# save to files
+print_function_values_to_file(front, outputPath+repoName+'/FUN.NSGAII.SearchRO')
+print_variables_to_file(front, outputPath+repoName+'/VAR.NSGAII.SearchRO')
+
+print('Algorithm (continuous problem): ' + algorithm.get_name())
+print('Problem: ' + problem.get_name())
+print('Computing time: ' + str(algorithm.total_computing_time))
---
+++ jMetal/monoObjectiveRS.py
@@ -0,0 +1,120 @@
+from jmetal.core.problem import IntegerProblem
+from jmetal.core.solution import IntegerSolution
+import random
+from CodeOwnership.CodeOwnership import  CodeOwnership
+from Encoding.IntegerEncoding import IntegerEncoding
+from RefactoringOperation.RefactoringOperationDispatcher import dispatch
+from QMOOD.Qmood import Qmood
+import copy
+
+class MonoObjectiveRS(IntegerProblem):
+    def __init__(self,projectInfo,repoPath):
+        '''
+        set basic parameters and encode current project
+        :param projectInfo: project being processed, should be a list of jClass
+        :param repository: repository of project being processed, used to do code ownership related manipulation,
+        should be a entity of class Repository
+        '''
+        super(SearchROProblemInteger,self).__init__()
+        "8 objectives: QMOOD 6 metrics + highest ownership+# of commiters"
+        # self.number_of_objectives = 8
+        self.number_of_objectives = 1
+        "Length represents chromosome length"
+        self.number_of_variables = 4
+        # todo: Research on what are contraints for
+        "No contraints"
+        self.number_of_constraints = 0
+        "number of chromosome"
+        self.number_of_choromosome = 30
+
+        'Qmood: maximize    code ownership: maximize'
+        self.obj_directions=[self.MAXIMIZE,
+                             self.MAXIMIZE,
+                             self.MAXIMIZE,
+                             self.MAXIMIZE,
+                             self.MAXIMIZE,
+                             self.MAXIMIZE,
+                             self.MAXIMIZE,
+                             self.MAXIMIZE]
+        # self.obj_directions=[self.MINIMIZE,
+        #                      self.MINIMIZE,
+        #                      self.MINIMIZE,
+        #                      self.MINIMIZE,
+        #                      self.MINIMIZE,
+        #                      self.MINIMIZE,
+        #                      self.MINIMIZE,
+        #                      self.MINIMIZE]
+        self.obj_labels=['Effectiveness',
+                         'Extendibility',
+                         'Flexibility',
+                         'Functionality',
+                         'Resusability',
+                         'Understandability',
+                         'HighestOwnership',
+                         'NumOfCommiters']
+        self.projectInfo = projectInfo
+        self.codeOwnership = CodeOwnership(repoPath)
+        self.integerEncoding = IntegerEncoding()
+        self.integerEncoding.encoding(self.projectInfo)
+        self.lower_bound=[1, 1, 1, 1] *self.number_of_choromosome
+        self.upper_bound=[self.integerEncoding.ROTypeNum,
+                          self.integerEncoding.classNum,
+                          self.integerEncoding.classNum,
+                          self.integerEncoding.N]*self.number_of_choromosome
+        # self.initial_front= []
+        # print(self.upper_bound)
+
+    def evaluate(self, solution: IntegerSolution) -> IntegerSolution:
+        # print(solution.variables)
+        'Decode and execute'
+        decodedIntegerSequences = self.integerEncoding.decoding(solution.variables)
+        "Execute corressponding refactoring operations"
+
+        projectInfo = copy.deepcopy(self.projectInfo)
+        for each in decodedIntegerSequences:
+            # print("Refactoring Operation: ", each)
+            dispatch(each["ROType"].value)(each, projectInfo)
+
+        'calculate QMOOD  after executed refactoring operations'
+        qmood = Qmood()
+        qmood.calculateQmood(projectInfo)
+        effectiveness = qmood.getEffectiveness()
+        extendibility = qmood.getExtendibility()
+        flexibility = qmood.getFlexibility()
+        functionality = qmood.getFunctionality()
+        resusability = qmood.getResusability()
+        understandability = qmood.getUnderstandability()
+
+        minus = -1.0
+
+        solution.objectives[0] = minus * effectiveness
+        # solution.objectives[1] = minus * extendibility
+        # solution.objectives[2] = minus * flexibility
+        # solution.objectives[3] = minus * functionality
+        # solution.objectives[4] = minus * resusability
+        # solution.objectives[5] = minus * understandability
+
+        # 'calculate ownership on refactoring operations applied files'
+        # highestOwnership, numOfCommiters = self.codeOwnership.calculateOwnership(decodedIntegerSequences)
+        # # print("highestOwnership: ",highestOwnership)
+        # # print("numOfCommiters: ",numOfCommiters)
+        # solution.objectives[6] = minus * highestOwnership
+        # solution.objectives[7] = minus * numOfCommiters
+
+        # self.initial_solution.append(solution.objectives)
+        return solution
+
+    def create_solution(self) -> IntegerSolution:
+        newSolution = IntegerSolution(lower_bound=self.lower_bound,
+                                      upper_bound=self.upper_bound,
+                                      number_of_objectives=self.number_of_objectives,
+                                      number_of_constraints=self.number_of_constraints)
+
+        newSolution.variables = \
+            [int(random.uniform(self.lower_bound[i] * 1.0, self.upper_bound[i] * 1.0))
+             for i in range(self.number_of_variables*self.number_of_choromosome)]
+        # self.initial_front.append(self.evaluate(newSolution))
+        return newSolution
+
+    def get_name(self) -> str:
+        return "Search Refactoring Operation Problem"
