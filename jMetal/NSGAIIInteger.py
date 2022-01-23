c78420f10b74e7aa46b5f1ab593aa78389489c3d
MashiroCl <441016116@qq.com>
2022-01-23 22:07:00 +0900

    Update: Change objectives to only 1 which is the sum of Qmood

--- jMetal/NSGAIIInteger.py
+++ jMetal/NSGAIIInteger.py
@@ -10,7 +10,8 @@ from Jxplatform2.jClass import jClass
 from SearchROProblemInteger import SearchROProblemInteger
 from jmetal.lab.visualization import Plot,InteractivePlot
 from jmetal.util.observer import WriteFrontToFileObserver,PlotFrontToFileObserver,ProgressBarObserver,BasicObserver
-
+from CodeOwnership.DeveloperGraph import DeveloperGraph
+from CodeOwnership.PullRequestService import PullRequestService
 
 'Read Jxplatform2 extraction result'
 # jsonFile = "/Users/leichen/Desktop/jedis.json"
@@ -22,8 +23,8 @@ platform = sys.argv[3]
 
 if platform == "1":
     'Local'
-    max_evaluations = 500
-    repoName = "ganttproject-1.10.2"
+    max_evaluations = 15000
+    repoName = "mbassador"
     jsonFile = "/Users/leichen/Desktop/" +repoName +".json"
     repoPath = "/Users/leichen/ResearchAssistant/InteractiveRebase/data/" + repoName
     outputPath = "/Users/leichen/Desktop/output"
@@ -34,46 +35,38 @@ elif platform == "2":
     repoPath = "/home/chenlei/MORCO/data/" + repoName
     outputPath = "/home/chenlei/MORCO/output_all_objectives_positive/"
 
-
+#load repository class info
 load = readJson(jsonFile)
 jClist = []
 for each in load:
     jClist.append(jClass(load=each))
-# print(jClist)
 
-problem = SearchROProblemInteger(jClist,repoPath)
+#load developer relationship
+relationshipCsvPath = "/Users/leichen/ResearchAssistant/InteractiveRebase/data/mbassador/MORCOoutput/csv/pullrequest.csv"
+res = PullRequestService().loadPullRequest(relationshipCsvPath)
+developerGraph = DeveloperGraph(res).generate_vertices().build()
+
+problem = SearchROProblemInteger(jClist,repoPath,developerGraph)
 
 # max_evaluations=5000
 algorithm = NSGAII(
     problem=problem,
-    population_size=50,
-    offspring_population_size=50,
-    mutation=IntegerPolynomialMutation(probability=0.15),
-    crossover=IntegerSBXCrossover(probability=0.8),
+    population_size=150,
+    offspring_population_size=140,
+    mutation=IntegerPolynomialMutation(probability=0.5),
+    crossover=IntegerSBXCrossover(probability=1),
     termination_criterion=StoppingByEvaluations(max_evaluations=int(max_evaluations))
 )
-# algorithm.observable.register(observer=PlotFrontToFileObserver('dynamic_front_vis'))
-# algorithm.observable.rfegister(observer=WriteFrontToFileObserver('dynamic_front'))
-# algorithm.observable.register(observer=WriteFrontToFileObserver(output_directory="/Users/leichen/Code/pythonProject/pythonProject/salabResearch/jMetal/FrontData"))
+
 algorithm.observable.register(observer=BasicObserver())
-# algorithm.observable.register(observer=ProgressBarObserver(max=max_evaluations))
 algorithm.observable.register(observer=WriteFrontToFileObserver(
-    output_directory=outputPath+repoName+"/front"))
+    output_directory=outputPath+repoName+"/front2"))
 algorithm.run()
-
-front = algorithm.get_result()
-# solutionObjectives = "/Users/leichen/Code/pythonProject/pythonProject/salabResearch/jMetal/solutionObjectives.txt"
-# with open(solutionObjectives,"w") as f:
-#     json.dump(problem.mySolutions,f)
-
-
-# problem.reference_front = problem.initial_front
-# plot_front = Plot(title='Pareto front approximation'
-#                   , axis_labels=problem.obj_labels
-#                   , reference_front=problem.reference_front)
-# plot_front.plot(front, label='NSGAII', filename=algorithm.get_name())
-
-
+front = get_non_dominated_solutions(algorithm.get_result())
+# front = problem.front
+# for each in algorithm.get_result():
+#     front.append(each)
+#
 # save to files
 print_function_values_to_file(front, outputPath+repoName+'/FUN.NSGAII.SearchRO')
 print_variables_to_file(front, outputPath+repoName+'/VAR.NSGAII.SearchRO')
--- jMetal/SearchROProblemInteger.py
+++ jMetal/SearchROProblemInteger.py
@@ -1,49 +1,37 @@
 from jmetal.core.problem import IntegerProblem
 from jmetal.core.solution import IntegerSolution
 import random
-from CodeOwnership.CodeOwnership import  CodeOwnership
+from CodeOwnership.CodeOwnership import CodeOwnership
+
 from Encoding.IntegerEncoding import IntegerEncoding
 from RefactoringOperation.RefactoringOperationDispatcher import dispatch
 from QMOOD.Qmood import Qmood
 import copy
 
 class SearchROProblemInteger(IntegerProblem):
-    def __init__(self,projectInfo,repoPath):
+    def __init__(self, projectInfo, repoPath, developerGraph):
         '''
         set basic parameters and encode current project
         :param projectInfo: project being processed, should be a list of jClass
         :param repository: repository of project being processed, used to do code ownership related manipulation,
-        should be a entity of class Repository
+        should be an entity of class Repository
         '''
         super(SearchROProblemInteger,self).__init__()
         "8 objectives: QMOOD 6 metrics + highest ownership+# of commiters"
-        self.number_of_objectives = 8
+        self.number_of_objectives = 1
         # self.number_of_objectives = 6
-        "Length represents chromosome length"
+        "4 variables decide a refactoring operation"
         self.number_of_variables = 4
         # todo: Research on what are contraints for
         "No contraints"
         self.number_of_constraints = 0
         "number of chromosome"
-        self.number_of_choromosome = 100
+        self.number_of_refactorings = 1
 
         'Qmood: maximize    code ownership: maximize'
         self.obj_directions=[self.MAXIMIZE,
-                             self.MAXIMIZE,
-                             self.MAXIMIZE,
-                             self.MAXIMIZE,
-                             self.MAXIMIZE,
-                             self.MAXIMIZE,
-                             self.MAXIMIZE,
                              self.MAXIMIZE]
-        # self.obj_directions=[self.MINIMIZE,
-        #                      self.MINIMIZE,
-        #                      self.MINIMIZE,
-        #                      self.MINIMIZE,
-        #                      self.MINIMIZE,
-        #                      self.MINIMIZE,
-        #                      self.MINIMIZE,
-        #                      self.MINIMIZE]
+
         self.obj_labels=['Effectiveness',
                          'Extendibility',
                          'Flexibility',
@@ -53,63 +41,55 @@ class SearchROProblemInteger(IntegerProblem):
                          'HighestOwnership',
                          'NumOfCommiters']
         self.projectInfo = projectInfo
-        self.codeOwnership = CodeOwnership(repoPath)
+        self.repoPath = repoPath
         self.integerEncoding = IntegerEncoding()
         self.integerEncoding.encoding(self.projectInfo)
-        self.lower_bound=[1, 1, 1, 1] *self.number_of_choromosome
+        self.lower_bound=[1, 1, 1, 1] *self.number_of_refactorings
         self.upper_bound=[self.integerEncoding.ROTypeNum,
                           self.integerEncoding.classNum,
                           self.integerEncoding.classNum,
-                          self.integerEncoding.N]*self.number_of_choromosome
-        # self.initial_front= []
-        # print(self.upper_bound)
+                          self.integerEncoding.N]*self.number_of_refactorings
+        self.initial_objectives = Qmood().calculateQmood(self.projectInfo)
+        self.developerGraph = developerGraph
 
     def evaluate(self, solution: IntegerSolution) -> IntegerSolution:
-        # print(solution.variables)
         'Decode and execute'
         decodedIntegerSequences = self.integerEncoding.decoding(solution.variables)
         "Execute corressponding refactoring operations"
 
         projectInfo = copy.deepcopy(self.projectInfo)
         for each in decodedIntegerSequences:
-            # print("Refactoring Operation: ", each)
             dispatch(each["ROType"].value)(each, projectInfo)
 
         'calculate QMOOD  after executed refactoring operations'
-        qmood = Qmood()
-        qmood.calculateQmood(projectInfo)
-        effectiveness = qmood.getEffectiveness()
-        extendibility = qmood.getExtendibility()
-        flexibility = qmood.getFlexibility()
-        functionality = qmood.getFunctionality()
-        resusability = qmood.getResusability()
-        understandability = qmood.getUnderstandability()
+        qmood_metrics_value = Qmood().calculateQmood(projectInfo)
 
-        minus = -1.0
+        minus = -1
+        qmood_metrics_list = ["Effectiveness", "Extendibility", "Flexibility", "Functionality", "Resusability",
+                              "Understandability"]
+        # qmood_metrics_list = ["Resusability"]
 
-        solution.objectives[0] = minus * effectiveness
-        solution.objectives[1] = minus * extendibility
-        solution.objectives[2] = minus * flexibility
-        solution.objectives[3] = minus * functionality
-        solution.objectives[4] = minus * resusability
-        solution.objectives[5] = minus * understandability
+        # print(solution.variables,end=" ")
+        # code_quality = 0
+        # for i in range(len(qmood_metrics_list)):
+        #     if i==0:
+        #         code_quality += -1*(qmood_metrics_value[qmood_metrics_list[i]]-self.initial_objectives[qmood_metrics_list[i]])
+        #     else:
+        #         code_quality += -1*0.01*(qmood_metrics_value[qmood_metrics_list[i]]-self.initial_objectives[qmood_metrics_list[i]])
 
-        # solution.objectives[0] = minus * effectiveness if effectiveness<0 else effectiveness
-        # solution.objectives[1] = minus * extendibility if extendibility<0 else extendibility
-        # solution.objectives[2] = minus * flexibility if flexibility<0 else flexibility
-        # solution.objectives[3] = minus * functionality if functionality<0 else functionality
-        # solution.objectives[4] = minus * resusability if resusability<0 else resusability
-        # solution.objectives[5] = minus * understandability if understandability<0 else understandability
-        # print(solution)
+        # print([-1*(qmood_metrics_value[metric] - self.initial_objectives[metric]) for metric in qmood_metrics_list])
+        # print(minus * sum([qmood_metrics_value[metric] - self.initial_objectives[metric] for metric in qmood_metrics_list]))
 
-        # 'calculate ownership on refactoring operations applied files'
-        highestOwnership, numOfCommiters = self.codeOwnership.calculateOwnership(decodedIntegerSequences)
-        # # print("highestOwnership: ",highestOwnership)
-        # # print("numOfCommiters: ",numOfCommiters)
-        solution.objectives[6] = minus * highestOwnership
-        solution.objectives[7] = minus * numOfCommiters
+        solution.objectives[0] = minus * sum([(qmood_metrics_value[metric] - self.initial_objectives[metric]) for metric in qmood_metrics_list])
+        # solution.objectives[0] = minus * (qmood_metrics_value["Understandability"] - self.initial_objectives["Understandability"])
 
-        # self.initial_solution.append(solution.objectives)
+        # print(code_quality)
+        # solution.objectives[0] = code_quality
+
+        'calculate ownership on refactoring operations applied files'
+        # relationship = CodeOwnership(self.repoPath).findAuthorPairList(decodedIntegerSequences).calculateRelationship(self.developerGraph)
+        # solution.objectives[1] = minus * relationship
+        # solution.objectives[1] = 0
         return solution
 
     def create_solution(self) -> IntegerSolution:
@@ -117,11 +97,9 @@ class SearchROProblemInteger(IntegerProblem):
                                       upper_bound=self.upper_bound,
                                       number_of_objectives=self.number_of_objectives,
                                       number_of_constraints=self.number_of_constraints)
-
         newSolution.variables = \
             [int(random.uniform(self.lower_bound[i] * 1.0, self.upper_bound[i] * 1.0))
-             for i in range(self.number_of_variables*self.number_of_choromosome)]
-        # self.initial_front.append(self.evaluate(newSolution))
+             for i in range(self.number_of_variables*self.number_of_refactorings)]
         return newSolution
 
     def get_name(self) -> str:
