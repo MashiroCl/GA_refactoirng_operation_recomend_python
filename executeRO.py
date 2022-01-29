6f5ddc04076d16d6395678d4da1ecca53fa9b46c
MashiroCl <441016116@qq.com>
2022-01-29 22:35:57 +0900

    Update: remove executeRO.py

--- executeRO.py
+++
@@ -1,7 +0,0 @@
-#Use result of Jxplatform2 to simulate moving method
-class ExecuteRO():
-    def __init__(self):
-        pass
-    def moveMethod(self,jMethod,jClass1,jClass2):
-        jClass1.removeMethod(jMethod)
-        jClass2.addMethod(jMethod)
