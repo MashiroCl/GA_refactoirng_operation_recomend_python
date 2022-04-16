4ffc4156e074540e7831129a19ff03938f6e7d10
MashiroCl <441016116@qq.com>
2022-04-16 15:39:56 +0900

    update: tests for call graph calculation

---
+++ tests/call_graph/CallGraphTest.py
@@ -0,0 +1,51 @@
+import unittest
+from call_graph.CallGraph import CallGraph
+
+class CallGraphTest(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(CallGraphTest, self).__init__( *args, **kwargs)
+
+
+    def test_CallGraph(self):
+        repo = "/Users/leichen/ResearchAssistant/InteractiveRebase/data/mbassador/MORCOoutput/csv/callgraph.json"
+        callgraph = CallGraph(repo)
+        # print(callgraph.callgraph)
+        print(callgraph.callgraph["test/java/net/engio/mbassy/StrongConcurrentSetTest.java"]["fieldAccessIn"])
+
+
+    def test_count_share_normal(self):
+        lista=[2,3]
+        listb=[2,2,6]
+        actual = CallGraph.count_share(lista,listb)
+        self.assertEqual(actual, 3/5)
+
+    def test_count_share_all_same(self):
+        lista=[2]
+        listb=[2,2,2]
+        actual = CallGraph.count_share(lista,listb)
+        self.assertEqual(actual, 1)
+
+    def test_count_share_all_difference(self):
+        lista=[2, 2]
+        listb=[3, 4, 5]
+        actual = CallGraph.count_share(lista,listb)
+        self.assertEqual(actual, 0)
+
+    def test_shared_call_out(self):
+        repo = "/Users/leichen/Desktop/student.json"
+        callgraph = CallGraph(repo)
+        print(callgraph.callgraph)
+        class1 = "Teacher.java"
+        class2 = "SchoolDay.java"
+        actual = callgraph.shared_call_out(class1, class2)
+        self.assertEqual(1,actual)
+
+
+    def test_shared_call_in_all_different(self):
+        repo = "/Users/leichen/Desktop/student.json"
+        callgraph = CallGraph(repo)
+        print(callgraph.callgraph)
+        class1 = "People.java"
+        class2 = "Student.java"
+        actual = callgraph.shared_call_in(class1, class2)
+        self.assertEqual(0,actual)
