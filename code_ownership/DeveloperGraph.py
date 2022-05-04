fc49c686d7ed999d8aba35f20126f4458b6c4760
MashiroCl <441016116@qq.com>
2022-05-04 22:34:53 +0900

    Update: add exclude list normalize relationship score

--- code_ownership/DeveloperGraph.py
+++ code_ownership/DeveloperGraph.py
@@ -20,8 +20,10 @@ class DeveloperGraph():
         'a vertive is one Developer'
         self.vertices=dict()
         'manually set baseline & deadline'
-        self.baseline = "2011-12-24"
+        self.baseline = "2008-12-24"
         self.deadline = "2022-12-24"
+        self.exclude_list = ["dependabot[bot]", ""]
+
 
     def set_bd_line(self, baseline, deadline):
         self.baseline = baseline
@@ -29,17 +31,18 @@ class DeveloperGraph():
 
     def generate_vertices(self):
         for each in self.pull_request_list:
-            if each.proposer not in self.vertices.keys():
+            if each.proposer not in self.vertices.keys() and each.proposer not in self.exclude_list :
                 self.vertices[each.proposer] = dict()
             for developer in each.commentators:
-                if developer not in self.vertices.keys():
+                if developer not in self.vertices.keys() and developer not in self.exclude_list:
                     self.vertices[developer] = dict()
         return self
 
     def update(self,value:float,vertice1,vertice2):
-        if vertice1!=vertice2:
+        if vertice1!=vertice2 and vertice1 not in self.exclude_list and vertice2 not in self.exclude_list:
             self.vertices[vertice1][vertice2] = self.vertices[vertice1].get(vertice2, 0) + value
             self.vertices[vertice2][vertice1] = self.vertices[vertice2].get(vertice1, 0) + value
+
 
     def build(self):
         for each_pull_request in self.pull_request_list:
@@ -50,15 +53,20 @@ class DeveloperGraph():
                 value = self.calculate_edges(i,each_pull_request)
                 'update both developer edge value'
                 self.update(value,each_pull_request.proposer,each_pull_request.commentators[i])
+
+
         'for relationship between authour and him/herself, set the highest value that appears in self.vertices'
         maximum =0
         for each in self.vertices:
-            # print(each)
-            # print(len(self.vertices[each].values()))
             if len(self.vertices[each].values())!=0:
-                maximum = max(max(self.vertices[each].values()),maximum)
+                maximum = max(max(self.vertices[each].values()), maximum)
         for each in self.vertices:
             self.vertices[each][each] = maximum
+
+        'normalize between (0,1]'
+        for dev1 in self.vertices:
+            for dev2 in self.vertices[dev1]:
+                self.vertices[dev1][dev2] /= maximum
 
         return self
 
