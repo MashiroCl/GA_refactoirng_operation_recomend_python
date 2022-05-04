826703987bbd59df9ac6b7fcc02b87272a640b52
MashiroCl <441016116@qq.com>
2022-05-04 16:46:33 +0900

    Feat: add fuzzy compare to unified the change of developer name (to lowercase and no space in names)

--- code_ownership/CodeOwnership.py
+++ code_ownership/CodeOwnership.py
@@ -61,10 +61,23 @@ class CodeOwnership:
         for each in self.authorPairList:
             developerA = each[0]
             developerB = each[1]
-            if developerA in developerGraph.vertices.keys():
-                if developerB in developerGraph.vertices[developerA].keys():
-                    relationship += developerGraph.vertices[developerA][developerB]
+            relationship += self.fuzzy_compare(developerA, developerB, developerGraph)
         return relationship/(len(self.authorPairList) if len(self.authorPairList)!=0 else 1)
+
+
+    def name_process(self, name:str):
+        return name.strip().replace(" ", "").replace("-","").lower()
+
+    def fuzzy_compare(self, devA, devB, developerGraph):
+        '''
+        check if devA and devB is in developerGraph or not
+        '''
+        devA = self.name_process(devA)
+        devB = self.name_process(devB)
+        if devA in developerGraph.vertices.keys():
+            if devB in developerGraph.vertices[devA].keys():
+                return developerGraph.vertices[devA][devB]
+        return 0
 
 
     @DeprecationWarning
@@ -108,3 +121,12 @@ class CodeOwnership:
         numOfCommiters = 1/commitersNum
 
         return highestOwenership, numOfCommiters
+
+if __name__=="__main__":
+    repoPath = "/Users/leichen/ResearchAssistant/InteractiveRebase/data/rhino"
+    csvPath = os.path.join(repoPath, "MORCOoutput","ownership.csv")
+    commitOutputPath = os.path.join(repoPath, "MORCOoutput")
+    csvOutputPath = os.path.join(repoPath, "MORCOoutput", "csv")
+    csvName = "ownership.csv"
+    localPath = "/Users/leichen/ResearchAssistant/InteractiveRebase/data"
+    Repository(repoPath).countAuthorCommit(commitOutputPath).authorCommitDict2CSV(csvOutputPath, csvName,localPath)
