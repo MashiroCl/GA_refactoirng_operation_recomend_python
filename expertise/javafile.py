9e340dd6a9b8afd7d11cdb33b83a4a014346b208
Lei Chen <mashirocl@gmail.com>
2022-11-20 20:39:53 +0900

    Update: extract method mkdir()

--- expertise/javafile.py
+++ expertise/javafile.py
@@ -3,6 +3,7 @@ import os
 import json
 from typing import List
 from expertise.commit import Commit
+from utils.directory import mkdir
 
 
 PRETTY_FORMAT = "--pretty=format:\'{%n  \"commit\": \"%H\",%n \"author\": {%n    \"name\": \"%aN\",%n    \"email\": \"%aE\",%n    \"date\": \"%aD\"%n  },%n  \"commiter\": {%n    \"name\": \"%cN\",%n    \"email\": \"%cE\",%n    \"date\": \"%cD\"%n  }%n},\'"
@@ -34,8 +35,7 @@ class File:
 
     def commits2csv(self):
         git_log_directory = os.path.join(self.repo_path, "MORCoRE")
-        if not os.path.exists(git_log_directory):
-            os.mkdir(git_log_directory)
+        mkdir(git_log_directory)
         command = " ".join(["cd", self.repo_path, "&&", "git", "--no-pager","log", PRETTY_FORMAT,
                             "--follow", self.path, ">", os.path.join(git_log_directory, self.name+".json")])
         # command = " ".join(["cd", self.repo_path, "&&", "git", "--no-pager","log", PRETTY_FORMAT, "--follow", self.path])
