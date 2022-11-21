ac815150a5129bb491ef33c70e83b5e149908ef1
Lei Chen <mashirocl@gmail.com>
2022-11-21 11:03:58 +0900

    Update: add platform valkyrie

--- search_technique/enviroment/Platform.py
+++ search_technique/enviroment/Platform.py
@@ -8,6 +8,7 @@ Titan: server titan,
 from call_graph.CallGraph import CallGraph
 from code_ownership.DeveloperGraph import DeveloperGraph
 from code_ownership.PullRequestService import PullRequestService
+from collaboration.graph import Graph
 
 
 class Platform:
@@ -15,33 +16,38 @@ class Platform:
         self.name = "platform"
         self.json_file_path = ""
         self.output_path = ""
-        #developer relationship
-        self.relationship_csv_path=""
-        self.ownership_path=""
-        self.repo_path=""
-        self.call_graph_path=""
-
+        # developers collaboration
+        self.collaboration_csv_path = ""
+        self.ownership_path = ""
+        self.repo_path = ""
+        self.call_graph_path = ""
 
     def __str__(self):
         return f"name:{self.name}\n" \
                f"json file path:{self.json_file_path}\n" \
                f"output path:{self.output_path}\n" \
-               f"relationship csv path:{self.relationship_csv_path}\n" \
+               f"relationship csv path:{self.collaboration_csv_path}\n" \
                f"ownership csv file path:{self.ownership_path}\n" \
                f"repository path:{self.repo_path}\n" \
                f"call graph csv file path:{self.call_graph_path}"
 
-    def set_repository(self,repo_name):
+    def set_repository(self, repo_name):
         '''
         Set repository path in paths of platform
         '''
 
-    def load_call_graph(self)->CallGraph:
+    def load_call_graph(self) -> CallGraph:
         return CallGraph(self.call_graph_path)
 
-    def load_developer_graph(self)->DeveloperGraph:
-        pullrequests = PullRequestService().loadPullRequest(self.relationship_csv_path)
+    def old_load_developer_graph(self) -> DeveloperGraph:
+        pullrequests = PullRequestService().loadPullRequest(self.collaboration_csv_path)
         return DeveloperGraph(pullrequests).generate_vertices().build()
+
+    def load_developer_graph(self) -> Graph:
+        g = Graph()
+        csv_path = self.collaboration_csv_path
+        g.build_from_csv(csv_path)
+        return g
 
 
 class LocalPlatform(Platform):
@@ -52,19 +58,32 @@ class LocalPlatform(Platform):
         self.json_file_path = "/Users/leichen/Desktop/StaticalAnalysis/" + repo_name + ".json"
         self.repo_path = "/Users/leichen/ResearchAssistant/InteractiveRebase/data/" + repo_name
         self.output_path = "/Users/leichen/Desktop/output/"
-        self.relationship_csv_path = "/Users/leichen/ResearchAssistant/InteractiveRebase/data/" + repo_name + "/MORCOoutput/csv/pullrequest.csv"
-        self.ownership_path = "/Users/leichen/ResearchAssistant/InteractiveRebase/data/" + repo_name + "/MORCOoutput/csv/ownership.csv"
+        self.collaboration_csv_path = "/Users/leichen/ResearchAssistant/InteractiveRebase/data/" + repo_name + "/MORCOoutput/csv/pullrequest.csv"
+        self.ownership_path = "/Users/leichen/ResearchAssistant/InteractiveRebase/data/" + repo_name + "/MORCOoutput/csv/owners.csv"
         self.call_graph_path = "/Users/leichen/ResearchAssistant/InteractiveRebase/data/" + repo_name + "/MORCOoutput/csv/callgraph.json"
 
 
 class TitanPlatform(Platform):
     def __init__(self):
-        self.name ="titan"
+        self.name = "titan"
 
-    def set_repository(self,repo_name):
+    def set_repository(self, repo_name):
         self.json_file_path = "/home/chenlei/MORCO/extractResult/" + repo_name + ".json"
         self.repo_path = "/home/chenlei/MORCO/data/" + repo_name
         self.output_path = "/home/chenlei/MORCO/output_temp/"
-        self.relationship_csv_path = "/home/chenlei/MORCO/relationship/" + repo_name + "/pullrequest.csv"
-        self.ownership_path = "/home/chenlei/MORCO/relationship/" + repo_name + "/ownership.csv"
+        self.collaboration_csv_path = "/home/chenlei/MORCO/relationship/" + repo_name + "/pullrequest.csv"
+        self.ownership_path = "/home/chenlei/MORCO/relationship/" + repo_name + "/owners.csv"
         self.call_graph_path = "/home/chenlei/MORCO/relationship/" + repo_name + "/callgraph.json"
+
+
+class ValkyriePlatform(Platform):
+    def __init__(self):
+        self.name = "valkyrie"
+
+    def set_repository(self, repo_name):
+        self.json_file_path = "/home/chenlei/projects/master_thesis/dataset/mailmapBuilt/" + repo_name + "MORECoRE/csv/abs.json"
+        self.repo_path = "/home/chenlei/projects/master_thesis/dataset/mailmapBuilt/" + repo_name
+        self.output_path = "/home/chenlei/projects/master_thesis/dataset/mailmapBuilt/" + repo_name + "MORECoRE/output/"
+        self.collaboration_csv_path = "/home/chenlei/projects/master_thesis/dataset/mailmapBuilt/" + repo_name + "MORECoRE/csv/pullrequest.csv"
+        self.ownership_path = "/home/chenlei/projects/master_thesis/dataset/mailmapBuilt/" + repo_name + "MORECoRE/csv/owners.csv"
+        self.call_graph_path = "/home/chenlei/projects/master_thesis/dataset/mailmapBuilt/" + repo_name + "MORECoRE/csv/call.json"
