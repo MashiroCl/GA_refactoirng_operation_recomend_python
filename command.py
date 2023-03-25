dfe7c9cd68d316020b43449c5dc6d74e1b752a25
Lei Chen <mashirocl@gmail.com>
2023-03-25 14:18:50 +0900

    update: run command and platform paths

--- command.py
+++ command.py
@@ -1,8 +1,10 @@
-import argparse, os
+import argparse, os, datetime
 from expertise.ownership import get_repo_ownership_t, extract_owners, get_repo_ownership_t_without_similar_files
 from collaboration.collaboration import get_pr_history
 from jxplatform2.Jxplatform2 import extract_abs, extract_call_graph
 from utils.directory import mkdir
+from expertise.build_table import build_expertise_table, load_expertise_table
+from workload.workload import cal_workload, get_workload_dict, store_workload, load_workload
 from search_technique.Nsga3RE import Nsga3RE
 from search_technique.Nsga3NRE import Nsga3NRE
 from search_technique.NsgaiiRE import NsgaiiRE
@@ -74,6 +76,17 @@ def extract_repo_model(jxplatform, repo_p, output_directory):
     extract_call_graph(jxplatform, repo_p, call_p)
 
 
+def extract_workload_expertise(repo_path, repo_url, csv_p, end_point, period):
+    build_expertise_table(repo_path, csv_p + "expertise_table.csv")
+    expertise_table = load_expertise_table(csv_p + "expertise_table.csv")
+    reviewers = expertise_table['Unnamed: 0'].tolist()
+    workload = cal_workload(repo_url, end_point, period)
+    store_workload(workload, csv_p + "temporary_workload.json")
+    # Manually unifying the names
+    input(f"Please unify the names in workload table {csv_p + 'temporary_workload.json'}, after unification, press enter")
+    d = get_workload_dict(load_workload(csv_p + "temporary_workload.json"), reviewers)
+    store_workload(d, csv_p + "workload_table.json")
+
 def extract(args):
     jxplatform = "jxplatform2/arExtractor.jar"
 
@@ -91,9 +104,15 @@ def extract(args):
     # extract_repo_model(jxplatform, repo_p, csv_p)
     # print(f"Finished extracting repository model for {repo_p}")
 
-    print(f"Extracting expertise for {repo_p}")
-    extract_expertise(repo_p, csv_p)
-    print(f"Finished extracting expertise for {repo_p}")
+    print(f"Extracting expertise_workload model for {repo_p}")
+    end_point = datetime.datetime(2023, 3, 18)
+    period = datetime.timedelta(days=7)
+    extract_workload_expertise(repo_p, repo_url, csv_p, end_point, period)
+    print(f"Finished extracting expertise_workload model for {repo_p}")
+
+    # print(f"Extracting expertise for {repo_p}")
+    # extract_expertise(repo_p, csv_p)
+    # print(f"Finished extracting expertise for {repo_p}")
 
     # print(f"Extracting collaboration for {repo_p}")
     # extract_collaboration(repo_url, csv_p)
@@ -136,17 +155,18 @@ def search_with_output_num(args):
     platform = args.p
     output_num = args.d
 
-    algorithms = [Nsga3RE(),
-                  Nsga3NRE(),
+    algorithms = [
+                # Nsga3RE(),
+                #   Nsga3NRE(),
                   NsgaiiRE(),
                   NsgaiiNRE(),
-                  RandomSearchRE(),
-                  SPEA2RE(),
-                  SPEA2NRE(),
-                  IBEARE(),
-                  IBEANRE(),
-                  MOCellRE(),
-                  MOCellNRE()
+                  # RandomSearchRE(),
+                  # SPEA2RE(),
+                  # SPEA2NRE(),
+                  # IBEARE(),
+                  # IBEANRE(),
+                  # MOCellRE(),
+                  # MOCellNRE()
                   ]
 
     for algorithm in algorithms:
--- search_technique/enviroment/Platform.py
+++ search_technique/enviroment/Platform.py
@@ -9,6 +9,8 @@ from call_graph.CallGraph import CallGraph
 from code_ownership.DeveloperGraph import DeveloperGraph
 from code_ownership.PullRequestService import PullRequestService
 from collaboration.graph import Graph
+from expertise.build_table import load_expertise_table, WorkloadExpertise
+from workload.workload import load_workload
 
 
 class Platform:
@@ -18,19 +20,25 @@ class Platform:
         self.json_file_path = ""
         self.output_path = ""
         # developers collaboration
-        self.collaboration_csv_path = ""
-        self.ownership_path = ""
+        # self.collaboration_csv_path = ""
+        # self.ownership_path = ""
         self.repo_path = ""
         self.call_graph_path = ""
+
+        # expertise_workload
+        self.workload_table_path = ""
+        self.expertise_table_path = ""
 
     def __str__(self):
         return f"name:{self.name}\n" \
                f"json file path:{self.json_file_path}\n" \
                f"output path:{self.output_path}\n" \
-               f"relationship csv path:{self.collaboration_csv_path}\n" \
-               f"ownership csv file path:{self.ownership_path}\n" \
+               f"workload_table path:{self.workload_table_path}\n" \
+               f"expertise_table path:{self.expertise_table_path}\n" \
                f"repository path:{self.repo_path}\n" \
                f"call graph csv file path:{self.call_graph_path}"
+        # f"relationship csv path:{self.collaboration_csv_path}\n" \
+        # f"ownership csv file path:{self.ownership_path}\n" \
 
     def set_repository(self, repo_name):
         '''
@@ -41,50 +49,51 @@ class Platform:
     def load_call_graph(self) -> CallGraph:
         return CallGraph(self.call_graph_path, self.repo_name)
 
-    def old_load_developer_graph(self) -> DeveloperGraph:
-        pullrequests = PullRequestService().loadPullRequest(self.collaboration_csv_path)
-        return DeveloperGraph(pullrequests).generate_vertices().build()
+    # def old_load_developer_graph(self) -> DeveloperGraph:
+    #     pullrequests = PullRequestService().loadPullRequest(self.collaboration_csv_path)
+    #     return DeveloperGraph(pullrequests).generate_vertices().build()
+    #
+    # def load_developer_graph(self) -> Graph:
+    #     g = Graph()
+    #     csv_path = self.collaboration_csv_path
+    #     g.build_from_csv(csv_path)
+    #     return g
 
-    def load_developer_graph(self) -> Graph:
-        g = Graph()
-        csv_path = self.collaboration_csv_path
-        g.build_from_csv(csv_path)
-        return g
+    def load_workload_expertise(self) -> WorkloadExpertise:
+        return WorkloadExpertise(load_expertise_table(self.expertise_table_path),
+                                 load_workload(self.workload_table_path))
 
 
 class LocalPlatform(Platform):
     def __init__(self):
+        super().__init__()
         self.name = "local"
 
     def set_repository(self, repo_name):
         self.repo_name = repo_name
-        self.json_file_path = "/Users/leichen/experiement_result/MORCoRE2/infos/" + repo_name + "/csv/abs.json"
+        self.json_file_path = "/Users/leichen/experiement_result/MORCoRA/infos/" + repo_name + "/csv/abs.json"
         self.repo_path = "/Users/leichen/ResearchAssistant/InteractiveRebase/data/" + repo_name
         self.output_path = "/Users/leichen/Desktop/output/"
-        self.collaboration_csv_path = "/Users/leichen/experiement_result/MORCoRE2/infos/" + repo_name + "/csv/pullrequest.csv"
-        self.ownership_path = "/Users/leichen/experiement_result/MORCoRE2/infos/" + repo_name + "/csv/owners.csv"
-        self.call_graph_path = "/Users/leichen/experiement_result/MORCoRE2/infos/" + repo_name + "/csv/call.json"
+        self.expertise_table_path = "/Users/leichen/experiement_result/MORCoRA/infos/" + repo_name + "/csv/expertise_table.csv"
+        self.workload_table_path = "/Users/leichen/experiement_result/MORCoRA/infos/" + repo_name + "/csv/workload_table.json"
+        # self.collaboration_csv_path = "/Users/leichen/experiement_result/MORCoRA/infos/" + repo_name + "/csv/pullrequest.csv"
+        # self.ownership_path = "/Users/leichen/experiement_result/MORCoRA/infos/" + repo_name + "/csv/owners.csv"
+        self.call_graph_path = "/Users/leichen/experiement_result/MORCoRA/infos/" + repo_name + "/csv/call.json"
 
 
 class TitanPlatform(Platform):
     def __init__(self):
+        super().__init__()
         self.name = "titan"
 
     def set_repository(self, repo_name):
-        # self.repo_name = repo_name
-        # self.json_file_path = "/home/chenlei/MORCO/extractResult/" + repo_name + ".json"
-        # self.repo_path = "/home/chenlei/MORCO/data/" + repo_name
-        # self.output_path = "/home/chenlei/MORCO/output_temp/"
-        # self.collaboration_csv_path = "/home/chenlei/MORCO/relationship/" + repo_name + "/pullrequest.csv"
-        # self.ownership_path = "/home/chenlei/MORCO/relationship/" + repo_name + "/owners.csv"
-        # self.call_graph_path = "/home/chenlei/MORCO/relationship/" + repo_name + "/callgraph.json"
-        root = "/home/chenlei/MORCoRE/dataset/"
+        root = "/home/salab/chenlei/MORCoRA/dataset/"
         self.repo_name = repo_name
         self.json_file_path = root + repo_name + "/csv/abs.json"
         self.repo_path = root + repo_name
         self.output_path = root + repo_name + "/output/"
-        self.collaboration_csv_path = root + repo_name + "/csv/pullrequest.csv"
-        self.ownership_path = root + repo_name + "/csv/owners.csv"
+        self.expertise_table_path = root + repo_name + "/csv/expertise_table.csv"
+        self.workload_table_path = root + repo_name + "/csv/workload_table.json"
         self.call_graph_path = root + repo_name + "/csv/call.json"
 
 
@@ -93,12 +102,15 @@ class ValkyriePlatform(Platform):
         self.name = "valkyrie"
 
     def set_repository(self, repo_name):
+        root = ""
         self.repo_name = repo_name
         self.json_file_path = "/home/chenlei/projects/master_thesis/dataset/mailmapBuilt/" + repo_name + "/MORCoRE/csv/abs.json"
         self.repo_path = "/home/chenlei/projects/master_thesis/dataset/mailmapBuilt/" + repo_name
         self.output_path = "/home/chenlei/projects/master_thesis/dataset/mailmapBuilt/" + repo_name + "/MORCoRE/output/"
-        self.collaboration_csv_path = "/home/chenlei/projects/master_thesis/dataset/mailmapBuilt/" + repo_name + "/MORCoRE/csv/pullrequest.csv"
-        self.ownership_path = "/home/chenlei/projects/master_thesis/dataset/mailmapBuilt/" + repo_name + "/MORCoRE/csv/owners.csv"
+        self.expertise_table_path = "/home/chenlei/projects/master_thesis/dataset/mailmapBuilt/" + repo_name + "/csv/expertise_table.csv"
+        self.workload_table_path = "/home/chenlei/projects/master_thesis/dataset/mailmapBuilt/" + repo_name + "/csv/workload_table.json"
+        # self.collaboration_csv_path = "/home/chenlei/projects/master_thesis/dataset/mailmapBuilt/" + repo_name + "/MORCoRE/csv/pullrequest.csv"
+        # self.ownership_path = "/home/chenlei/projects/master_thesis/dataset/mailmapBuilt/" + repo_name + "/MORCoRE/csv/owners.csv"
         self.call_graph_path = "/home/chenlei/projects/master_thesis/dataset/mailmapBuilt/" + repo_name + "/MORCoRE/csv/call.json"
 
 
@@ -107,13 +119,13 @@ class ThorPlatform(Platform):
         self.name = "thor"
 
     def set_repository(self, repo_name):
-        root = "/home/salab/chenlei/project/MORCoRE/dataset/"
+        root = "/home/salab/chenlei/project/MORCoRA/dataset/csv"
         self.repo_name = repo_name
         self.json_file_path = root + repo_name + "/csv/abs.json"
         self.repo_path = root + repo_name
         self.output_path = root + repo_name + "/output/"
-        self.collaboration_csv_path = root + repo_name + "/csv/pullrequest.csv"
-        self.ownership_path = root + repo_name + "/csv/owners.csv"
+        self.expertise_table_path = root + repo_name + "/csv/expertise_table.csv"
+        self.workload_table_path = root + repo_name + "/csv/workload_table.json"
         self.call_graph_path = root + repo_name + "/csv/call.json"
 
 
@@ -131,6 +143,8 @@ class CustomizePlatform(Platform):
         self.json_file_path = root + repo_name + "/MORCoRE/csv/abs.json"
         self.repo_path = root + repo_name
         self.output_path = root + repo_name + "/MORCoRE/output/"
-        self.collaboration_csv_path = root + repo_name + "/MORCoRE/csv/pullrequest.csv"
-        self.ownership_path = root + repo_name + "/MORCoRE/csv/owners.csv"
+        self.expertise_table_path = root + repo_name + "/MORCoRE/csv/expertise_table.csv"
+        self.workload_table_path = root + repo_name + "/MORCoRE/csv/workload_table.json"
+        # self.collaboration_csv_path = root + repo_name + "/MORCoRE/csv/pullrequest.csv"
+        # self.ownership_path = root + repo_name + "/MORCoRE/csv/owners.csv"
         self.call_graph_path = root + repo_name + "/MORCoRE/csv/call.json"
